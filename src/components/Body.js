b8656334ba56fe390a2d36d9ba4b82e2b4b6c074
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-04-20 20:48:50 +0530

    new changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,7 @@ import { RESTRAU_URL } from "../utils/constants";
 import ShimmerUI from "./ShimmerUI";
 import Banner from "./Banner";
 import food from "./images/food.png";
+import TopRestrau from "./TopRestrau";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -20,15 +21,21 @@ const Body = () => {
     console.log(arr1);
     setListOfRestaurants(arr1);
   };
-  return  (
+  return (
     <div className="w-full h-full flex flex-col overflow-x-hidden">
       <div className="banner">
-      <div className="text-img relative">
-        <img src={food} alt="food" className="w-full object-cover h-62 lg:h-80" />
-        <h2 className="text-center px-20 sm:px-72 sm:text-2xl md:text-4xl lg:text-6xl lg:px-20 font-extrabold py-14 uppercase tracking-widest bg-white bg-opacity-30 absolute w-full sm:w-[80rem]">Delight your taste buds with our delicious creations!</h2>
-      </div>
-      
-        <Banner/>
+        <div className="text-img relative">
+          <img
+            src={food}
+            alt="food"
+            className="w-full object-cover h-62 lg:h-80"
+          />
+          <h2 className="text-center px-20 sm:px-72 sm:text-2xl md:text-4xl lg:text-6xl lg:px-20 font-extrabold py-14 uppercase tracking-widest bg-white bg-opacity-30 absolute w-full sm:w-[80rem]">
+            Delight your taste buds with our delicious creations!
+          </h2>
+        </div>
+        <Banner />
+        <TopRestrau/>
       </div>
     </div>
   );
---
+++ src/components/TopRestrau.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopRestrau = () => {
+  return (
+    <div className='top-Restrau w-full h-96 bg-[#D4AD01] mt-8'>
+        <h1></h1>
+    </div>
+  )
+}
+
+export default TopRestrau
